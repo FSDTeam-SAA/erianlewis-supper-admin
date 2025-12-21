a901cf10512e8775b4e949c0b44c1a4db888f89b
Tanvirgit07 <ahmmedtanvir009@gmail.com>
2025-12-22 04:32:25 +0600

    add

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,19 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get("next-auth.session-token-dashboard")?.value;
+// export function middleware(request: NextRequest) {
+//   const token = request.cookies.get("next-auth.session-token-dashboard")?.value;
 
-  const publicPaths = ["/login", "/api/auth"];
-  const path = request.nextUrl.pathname;
+//   const publicPaths = ["/login", "/api/auth"];
+//   const path = request.nextUrl.pathname;
 
-  if (!token && !publicPaths.some((p) => path.startsWith(p))) {
-    return NextResponse.redirect(new URL("/signin", request.url));
-  }
+//   if (!token && !publicPaths.some((p) => path.startsWith(p))) {
+//     return NextResponse.redirect(new URL("/signin", request.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
-};
+// export const config = {
+//   matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+// };
